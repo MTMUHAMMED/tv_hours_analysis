754b874d58df5adba2822a9075da4e1daddce111
MUHAMMED TAHIR MUHAMMED <106257655+MTMUHAMMED@users.noreply.github.com>
2024-10-10 17:44:19 +0100

    filtered to age 30 and above

--- main.R
+++ main.R
@@ -1,9 +1,10 @@
-library(tidyverse)#
+library(tidyverse)
 library(here)
 
 head(gss_cat)
-#View(gss_cat)
+View(gss_cat)
 tv_hour_table <- gss_cat |> 
+  filter(age >=30) |> 
   group_by(marital) |> 
   summarise(mean_tv_hours = mean(tvhours, na.rm = TRUE))
 
