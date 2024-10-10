cfe15550c0e98a1fc312ad764c3e371e11a55dd1
MUHAMMED TAHIR MUHAMMED <106257655+MTMUHAMMED@users.noreply.github.com>
2024-10-10 23:01:33 +0100

    Shorten name

--- main.R
+++ main.R
@@ -3,10 +3,10 @@ library(here)
 
 head(gss_cat)
 View(gss_cat)
-tv_hour_table <- gss_cat |> 
+tv_hour_tab <- gss_cat |> 
   filter(age < 30) |> 
   group_by(marital) |> 
   summarise(mean_tv_hours = mean(tvhours, na.rm = TRUE))
 
-write.csv(tv_hour_table, here("Tv_Hour_by_Marital.csv"))
+write.csv(tv_hour_tab, here("Tv_Hour_by_Marital.csv"))
 
