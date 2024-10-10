29b27519a7603b2fbe591be375c1f6674e28d72f
MUHAMMED TAHIR MUHAMMED <106257655+MTMUHAMMED@users.noreply.github.com>
2024-10-10 17:10:37 +0100

    started analysis

---
+++ main.R
@@ -0,0 +1,11 @@
+library(tidyverse)#
+library(here)
+
+head(gss_cat)
+#View(gss_cat)
+tv_hour_table <- gss_cat |> 
+  group_by(marital) |> 
+  summarise(mean_tv_hours = mean(tvhours, na.rm = TRUE))
+
+write.csv(tv_hour_table, here("Tv_Hour_by_Marital.csv"))
+
